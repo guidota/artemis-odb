a9e22b333285a69e212147c8e5b6eb71afdfbe32
guidota <guido@tamborindeguy.com.ar>
2021-01-26 19:22:28 -0300

    feat: keep who component changes (#8)

--- artemis-fluid/artemis-fluid-core-utility-sources/src/test/java/com/artemis/ChangeRegistry.java
+++ artemis-fluid/artemis-fluid-core-utility-sources/src/test/java/com/artemis/ChangeRegistry.java
@@ -1,40 +1,43 @@
 package com.artemis;
 
-import java.lang.Class;
-import java.lang.Integer;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public final class ChangeRegistry extends BaseSystem {
-  protected Map<Integer, Set<Class<? extends Component>>> toRemove = new HashMap();
+    private final Map<Integer, Set<Class<? extends Component>>> toRemove = new HashMap<>();
 
-  protected Map<Integer, Set<Component>> toMark = new HashMap();
+    private final Map<Integer, Set<Component>> toMark = new HashMap<>();
 
-  protected void initialize() {
-    E._changeRegistry=this;
-  }
+    private final Map<Class<? extends Component>, Set<Integer>> components = new HashMap<>();
 
-  public void processSystem() {
-    E._changeRegistry=this;
-    toRemove.clear();
-    toMark.clear();
-  }
+    protected void initialize() {
+        E._changeRegistry = this;
+    }
 
-  void remove(int entityId, Class componentClass) {
-    this.toRemove.computeIfAbsent(entityId, id -> new HashSet()).add(componentClass);
-  }
+    public void processSystem() {
+        E._changeRegistry = this;
+        toRemove.clear();
+        toMark.clear();
+    }
 
-  public Map<Integer, Set<Class<? extends Component>>> getRemoved() {
-    return this.toRemove;
-  }
+    void remove(int entityId, Class<? extends Component> componentClass) {
+        this.toRemove.computeIfAbsent(entityId, id -> new HashSet<>()).add(componentClass);
+        this.components.computeIfAbsent(componentClass, clazz -> new HashSet<>()).add(entityId);
+    }
 
-  void mark(int entityId, Component component) {
-    this.toMark.computeIfAbsent(entityId, id -> new HashSet()).add(component);
-  }
+    public Map<Integer, Set<Class<? extends Component>>> getRemoved() {
+        return this.toRemove;
+    }
 
-  public Map<Integer, Set<Component>> getMarked() {
-    return this.toMark;
-  }
+    void mark(int entityId, Component component) {
+        this.toMark.computeIfAbsent(entityId, id -> new HashSet<>()).add(component);
+        this.components.computeIfAbsent(component.getClass(), clazz -> new HashSet<>()).add(entityId);
+    }
+
+    public Map<Integer, Set<Component>> getMarked() {
+        return this.toMark;
+    }
+
+    public Optional<Set<Integer>> getWhoChange(Class<? extends Component> componentClass) {
+        return Optional.ofNullable(this.components.get(componentClass));
+    }
 }
